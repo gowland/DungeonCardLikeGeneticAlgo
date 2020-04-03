60538910273394949819404fb0af89398b8684c7
Robert Gowland <robert.gowland@sonova.com>
2020-04-03 15:41:34 -0400

    Refactor out generation class to set up logging

--- GeneticSolver/Solver.cs
+++ GeneticSolver/Solver.cs
@@ -4,10 +4,106 @@ using System.Linq;
 
 namespace GeneticSolver
 {
+    public class Generation<T>
+    {
+        private readonly IGenerationFactory<T> _generationFactory;
+        private readonly IGenomeEvalautor<T> _evaluator;
+
+        public Generation(IEnumerable<IGenome<T>> genomes, IGenerationFactory<T> generationFactory, IGenomeEvalautor<T> evaluator)
+        {
+            _generationFactory = generationFactory;
+            _evaluator = evaluator;
+            Genomes = genomes;
+        }
+
+        public IEnumerable<IGenome<T>> Genomes { get; }
+
+        public ScoredGeneration<T> Score()
+        {
+            return new ScoredGeneration<T>(_evaluator.GetFitnessResults(Genomes), _generationFactory, _evaluator);
+        }
+
+        public Generation<T> Mutate()
+        {
+            return new Generation<T>(
+                _generationFactory.MutateGenomes(Genomes),
+                _generationFactory,
+                _evaluator);
+        }
+
+        public Generation<T> Concat(Generation<T> other)
+        {
+            return new Generation<T>(Genomes.Concat(other.Genomes).ToArray(), _generationFactory, _evaluator);
+        }
+
+        public Generation<T> BreedPairs(int count)
+        {
+            return new Generation<T>(GetChildren(count), _generationFactory, _evaluator);
+        }
+
+        private IEnumerable<IGenome<T>> GetChildren(int count)
+        {
+            foreach (var pair in GetPairs(Genomes))
+            {
+                var children = _generationFactory.CreateChildren(count, pair.Item1, pair.Item2);
+                var worthyChildren = GetFitnessResultsDescending(children)
+                    .Take(2)
+                    .Select(r => r.Genome)
+                    .ToArray();
+
+                yield return worthyChildren[0];
+                yield return worthyChildren[1];
+            }
+        }
+
+        private IOrderedEnumerable<FitnessResult<T>> GetFitnessResultsDescending(IEnumerable<IGenome<T>> generation)
+        {
+            return _evaluator.GetFitnessResults(generation).OrderByDescending(r => r.Fitness);
+        }
+
+        private IEnumerable<Tuple<IGenome<T>, IGenome<T>>> GetPairs(IEnumerable<IGenome<T>> genomes)
+        {
+            var genomesArr = genomes.ToArray();
+            while (genomesArr.Length > 1)
+            {
+                var pair = genomesArr.Take(2).ToArray();
+                yield return new Tuple<IGenome<T>, IGenome<T>>(pair[0], pair[1]);
+
+                genomesArr = genomesArr.Skip(2).ToArray();
+            }
+        }
+    }
+
+    public class ScoredGeneration<T>
+    {
+        private readonly IGenerationFactory<T> _generationFactory;
+        private readonly IGenomeEvalautor<T> _evaluator;
+
+        public ScoredGeneration(IEnumerable<FitnessResult<T>> fitnessResults, IGenerationFactory<T> generationFactory, IGenomeEvalautor<T> evaluator)
+        {
+            _generationFactory = generationFactory;
+            _evaluator = evaluator;
+            FitnessResults = fitnessResults;
+        }
+
+        public IEnumerable<FitnessResult<T>> FitnessResults { get; }
+        public IEnumerable<IGenome<T>> Genomes => FitnessResults.Select(r => r.Genome);
+
+        public Generation<T> TakeBest(int count)
+        {
+            return new Generation<T>(
+                FitnessResults.OrderByDescending(r => r.Fitness)
+                    .Take(count)
+                    .Select(r => r.Genome),
+                _generationFactory,
+                _evaluator);
+        }
+    }
+
     public class Solver<T>
     {
         private readonly IGenerationFactory<T> _generationFactory;
-        private IGenomeEvalautor<T> _evaluator;
+        private readonly IGenomeEvalautor<T> _evaluator;
 
         public Solver(IGenerationFactory<T> generationFactory, IGenomeEvalautor<T> evaluator)
         {
@@ -17,6 +113,21 @@ namespace GeneticSolver
 
         public IGenome<T> Solve(int count, int iterations)
         {
+            int numberToKeep = HalfButEven(count);
+            var generation = new Generation<T>(_generationFactory.CreateGeneration(count), _generationFactory, _evaluator);
+
+            for (int generationNum = 0; generationNum < iterations; generationNum++)
+            {
+                Console.WriteLine($"Starting generation {generationNum}");
+                var scoredGeneration = generation.Score();
+                var keepers = scoredGeneration.TakeBest(numberToKeep);
+                var children = keepers.BreedPairs(2).Mutate();
+                generation = keepers.Concat(children);
+            }
+
+            return generation.Score().TakeBest(1).Genomes.First();
+
+/*
             // Produce an initial generation of Genomes using a random number generator.
             var generation = _generationFactory.CreateGeneration(count);
 
@@ -25,7 +136,7 @@ namespace GeneticSolver
                 // Determine the fitness of all of the Genomes.
                 // Determine which Genomes are allowed to reproduce.
                 var worthyGenomes = GetFitnessResultsDescending(generation)
-                    .Take(HalfButEven(count))
+                    .TakeBest(HalfButEven(count))
                     .Select(r => r.Genome)
                     .ToArray();
 
@@ -41,6 +152,7 @@ namespace GeneticSolver
             }
 
             return GetFitnessResultsDescending(generation).First().Genome;
+*/
         }
 
         private IOrderedEnumerable<FitnessResult<T>> GetFitnessResultsDescending(IEnumerable<IGenome<T>> generation)
