3793e60f4b8a3bedd6844df32aa0ee8c5c57fe42
Robert Gowland <robert.gowland@sonova.com>
2020-04-27 17:44:59 -0400

    Missing file from previous commit

---
+++ GeneticSolver/Interfaces/IPairingStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneticSolver.Interfaces
+{
+    public interface IPairingStrategy
+    {
+        IEnumerable<Tuple<T, T>> GetPairs<T>(IEnumerable<T> genomes);
+    }
+}
