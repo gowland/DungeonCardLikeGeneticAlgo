68ed8aa4e0fa2ea56d06648412ca8f3bd9c57d98
Robert Gowland <robert.gowland@sonova.com>
2020-05-23 12:06:44 -0400

    Store best results numbers

--- DungeonCardsWatcher/MainWindow.xaml.cs
+++ DungeonCardsWatcher/MainWindow.xaml.cs
@@ -11,3 +11,11 @@
         }
     }
 }
+
+/*
+Gold multipliers              38.2574, 35.0479, 32.4267
+Monster w/ weapon multipliers 35.8533, -58.9001, -26.1292
+Monster no weapon multipliers 4.2462, 2.1694, 2.4283
+Weapon w/ weapon multipliers  -24.2076, -27.1800, -52.8834
+Weapon no weapon multipliers  57.8609, 47.2535, 35.5468
+ */
